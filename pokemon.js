9df8fbec6b26135ead7ebc2791cd8a6156695ffe
Benjamin-Carr <B.1415927@gmail.com>
2018-10-18 22:55:08 +0000

    committing to pull--working on directives

--- pokemon.js
+++ pokemon.js
@@ -1,13 +1,24 @@
 /* global angular */
 angular.module('app', [])
     .controller('mainCtrl', mainCtrl)
-    .directive('pokebox', pokeBoxDirective);
+    .directive('myCustomer', function() {
+        return {
+            restrict: 'E',
+            scope: {
+                customerInfo: '=info'
+            },
+            templateUrl: 'my-customer-iso.html'
+        };
+    });
+// .directive('myCustomer', myCustomerDirective);
 
 function mainCtrl($scope) {
     $scope.test = "Charzard";
     $scope.monsters = [
-        { name: "Pikachu", sprite: "https://img.pokemondb.net/sprites/platinum/normal/pikachu.png" },
+        { name: "Pikachu", address: "https://img.pokemondb.net/sprites/platinum/normal/pikachu.png" },
     ];
+    $scope.naomi = { name: 'Naomi', address: '1600 Amphitheatre' };
+    $scope.igor = { name: 'Igor', address: '123 Somewhere' };
     // $scope.addNew = function(user) {
     //     if (!user.name && !user.email) {
     //         alert("Please enter either an email or a name");
@@ -27,18 +38,19 @@ function mainCtrl($scope) {
     // };
 }
 
-function pokeBoxDirective() {
-    return {
-        scope: {
-            pokemon: '=pokemon'
-        },
-        restrict: 'E',
-        replace: 'true',
-        template: (
-            '<div class="PokeBox square-content border rounder border-shadow">' +
-                '<img class="sprite" src={{pokemon.sprite}} alt="picture of {{pokemon.name}}>' +
-                '<p>{{pokemon.name}}*</p>' +
-            '</div>'
-        ),
-    };
-}
+
+// function pokeBoxDirective() {
+//     return {
+//         scope: {
+//             pokemon: '=pokemon'
+//         },
+//         restrict: 'E',
+//         replace: 'true',
+//         template: (
+//             '<div class="PokeBox square-content border rounder border-shadow">' +
+//             '<img class="sprite" src={{pokemon.sprite}} alt="picture of {{pokemon.name}}>' +
+//             '<p>{{pokemon.name}}*</p>' +
+//             '</div>'
+//         ),
+//     };
+// }

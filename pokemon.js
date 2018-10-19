35ff9e66c41d895fe234a6f9947460ca26b6dc79
Benjamin-Carr <B.1415927@gmail.com>
2018-10-19 09:36:35 +0000

    everything functioning, eleminated directives because they messed with scope

--- pokemon.js
+++ pokemon.js
@@ -10,12 +10,27 @@ angular.module('app').directive('pokeModal', pokeModalDirective);
 function mainCtrl($scope, $http) {
     $scope.test = "Charzard";
     $scope.monsters = [
-        { name: "Pikachu", sprite: "https://img.pokemondb.net/sprites/platinum/normal/pikachu.png" },
-        { name: "Pikachu", sprite: "https://img.pokemondb.net/sprites/platinum/normal/pikachu.png" },
-        { name: "Pikachu", sprite: "https://img.pokemondb.net/sprites/platinum/normal/pikachu.png" },
+        { name: "", sprite: "https://img.pokemondb.net/sprites/platinum/normal/pikachu.png" },
+        { name: "", sprite: "" },
+        { name: "Pikachu2", sprite: "https://img.pokemondb.net/sprites/platinum/normal/pikachu.png" },
+        { name: "Pikachu3", sprite: "https://img.pokemondb.net/sprites/platinum/normal/pikachu.png" },
     ];
-    $scope.naomi = { name: 'Naomi', sprite: '1600 Amphitheatre' };
-    $scope.igor = { name: 'Igor', sprite: '123 Somewhere' };
+    $scope.empty = [
+        "yes",
+        "yes",
+        "yes",
+        "yes",
+        "yes",
+        "yes",
+        "yes",
+    ];
+    $scope.currentPokemon;
+    $scope.currentIndex;
+    
+    $scope.setIndex = function(square) {
+        $scope.currentIndex = square;
+        console.log("currentIndex: " + $scope.currentIndex);
+    };
 
     $scope.onsubmit = function(form) {
         var val = $scope.pokemonName;
@@ -23,30 +38,26 @@ function mainCtrl($scope, $http) {
         $http.get(url).then(function(response) {
             console.log(response);
             $scope.pokedata = response.data;
+            $scope.currentPokemon = response.data;
         });
     };
     
-    $scope.addToTeam = function() {
-        alert("testy test");  
+    $scope.addToTeam = function(index) {
+        console.log("adding to team:");
+        console.log($scope.currentPokemon['name']);
+        console.log($scope.currentPokemon['sprites']['front_default']);
+        console.log($scope.currentIndex);
+        console.log($scope.monsters[$scope.currentIndex]);
+        $scope.monsters[$scope.currentIndex] = {name: $scope.currentPokemon['name'], sprite: $scope.currentPokemon['sprites']['front_default']};
+        $scope.empty[$scope.currentIndex] = "no";
+        console.log($scope.monsters[$scope.currentIndex]);
+        console.log("empty[" + $scope.currentIndex + "]: " + $scope.empty[$scope.currentIndex]);
+        $scope.clear();
     };
-}
-
-function pokeBoxDirective() {
-    return {
-        restrict: 'E',
-        scope: {
-            pokemon: '='
-        },
-        templateUrl: 'poke-box.html'
-    };
-}
-
-function pokeModalDirective() {
-    return {
-        restrict: 'E',
-        scope: {
-            index: '='
-        },
-        templateUrl: 'poke-modal.html'
+    
+    $scope.clear = function() {
+        $scope.pokedata = null;
+        $scope.currentPokemon = null;
+        $scope.pokemonName = "";
     }
 }
